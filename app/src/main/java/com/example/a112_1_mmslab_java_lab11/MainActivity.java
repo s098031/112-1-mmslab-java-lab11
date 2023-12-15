01b68f254935d6d1839f26e1f82537c8db9e0336
s098031 <tests098031@gmail.com>
2023-12-15 14:37:30 +0800

    20231215

---
+++ app/src/main/java/com/example/a112_1_mmslab_java_lab11/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a112_1_mmslab_java_lab11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/a112_1_mmslab_java_lab11/MyDBHelper.java
@@ -0,0 +1,4 @@
+package com.example.a112_1_mmslab_java_lab11;
+
+public class MyDBHelper {
+}
