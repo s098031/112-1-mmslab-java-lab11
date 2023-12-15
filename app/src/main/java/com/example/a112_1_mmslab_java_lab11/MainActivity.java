d4e6a6f75d36a22ca66dc2e4b4ed6318d1c53640
s098031 <tests098031@gmail.com>
2023-12-15 15:04:25 +0800

    20231215

--- app/src/main/java/com/example/a112_1_mmslab_java_lab11/MainActivity.java
+++ app/src/main/java/com/example/a112_1_mmslab_java_lab11/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.a112_1_mmslab_java_lab11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText ed_book,ed_price;
+    private Button btn_insert,btn_upgrade,btn_delete,btn_query;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private  ArrayList<String> items=new ArrayList<>();
+    private SQLiteDatabase dbrw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ed_book=findViewById(R.id.ed_book);
+        ed_price=findViewById(R.id.ed_price);
+        btn_insert=findViewById(R.id.btn_insert);
+        btn_upgrade=findViewById(R.id.btn_update);
+        btn_delete=findViewById(R.id.btn_delete);
+        btn_query=findViewById(R.id.btn_query);
+        listView=findViewById(R.id.listView);
+        adapter=new ArrayAdapter<>(this,
+                android.R.layout.simple_list_itme_1,items);
+        listView.setAdapter(adapter);
+        dbrw=new MyDBHelper(this).getWritableDatabase();
     }
+    
 }
--- app/src/main/java/com/example/a112_1_mmslab_java_lab11/MyDBHelper.java
+++ app/src/main/java/com/example/a112_1_mmslab_java_lab11/MyDBHelper.java
@@ -1,4 +1,24 @@
 package com.example.a112_1_mmslab_java_lab11;
 
-public class MyDBHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyDBHelper extends SQLiteOpenHelper {
+    private static final String name = "mdatabase.db";
+    private static final int version =1;
+    MyDBHelper(Context context){
+        super(context,name,null,version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        db.execSQL("CREAT TABLE mytable(book text PRIMARY KEY,price integer NOT NULL)";
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS myTable");
+        onCreate(db);
+    }
 }
