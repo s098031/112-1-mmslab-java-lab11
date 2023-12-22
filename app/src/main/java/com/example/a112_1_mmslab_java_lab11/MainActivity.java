17f6b13ef7d3d3d4d718c62611fc401b75d6acdf
s098031 <tests098031@gmail.com>
2023-12-22 14:26:25 +0800

    20231222

--- app/src/main/java/com/example/a112_1_mmslab_java_lab11/MainActivity.java
+++ app/src/main/java/com/example/a112_1_mmslab_java_lab11/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a112_1_mmslab_java_lab11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -9,12 +10,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private EditText ed_book,ed_price;
-    private Button btn_insert,btn_upgrade,btn_delete,btn_query;
+    private Button btn_insert,btn_update,btn_delete,btn_query;
     private ListView listView;
     private ArrayAdapter<String> adapter;
     private  ArrayList<String> items=new ArrayList<>();
@@ -26,14 +29,85 @@ public class MainActivity extends AppCompatActivity {
         ed_book=findViewById(R.id.ed_book);
         ed_price=findViewById(R.id.ed_price);
         btn_insert=findViewById(R.id.btn_insert);
-        btn_upgrade=findViewById(R.id.btn_update);
+        btn_update=findViewById(R.id.btn_update);
         btn_delete=findViewById(R.id.btn_delete);
         btn_query=findViewById(R.id.btn_query);
         listView=findViewById(R.id.listView);
         adapter=new ArrayAdapter<>(this,
-                android.R.layout.simple_list_itme_1,items);
+                android.R.layout.simple_list_item_1,items);
         listView.setAdapter(adapter);
         dbrw=new MyDBHelper(this).getWritableDatabase();
+
+        btn_insert.setOnClickListener(view -> {
+            if(ed_book.length()<1||ed_price.length()<1) Toast.makeText(MainActivity.this,"欄位請誤留空",Toast.LENGTH_SHORT).show();
+            else {
+                try{
+                    dbrw.execSQL("INSERT INTO myTable(book,price) values(?,?)",
+                            new Object[]{ed_book.getText().toString(),ed_price.getText().toString()});
+                    Toast.makeText(MainActivity.this,"新增書名"+ed_book.getText().toString()+"     價格"+ed_price.getText().toString(),Toast.LENGTH_SHORT).show();
+                    ed_book.setText("");
+                    ed_price.setText("");
+                }catch (Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this,"新增失敗"+e.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btn_update.setOnClickListener(view -> {
+            if(ed_book.length()<1||ed_price.length()<1) Toast.makeText(MainActivity.this,"欄位請誤留空",Toast.LENGTH_SHORT).show();
+            else {
+                try{
+                    dbrw.execSQL("UPDATE myTable SET price ="
+                            +ed_price.getText().toString()
+                            +" WHERE book LIKE '"
+                            +ed_book.getText().toString()+"'");
+                    Toast.makeText(MainActivity.this,"更新書名"+ed_book.getText().toString()+"     價格"+ed_price.getText().toString(),Toast.LENGTH_SHORT).show();
+                    ed_book.setText("");
+                    ed_price.setText("");
+                }catch (Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this,"更新失敗"+e.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btn_delete.setOnClickListener(view -> {
+            if(ed_book.length()<1) Toast.makeText(MainActivity.this,"書名請誤留空",Toast.LENGTH_SHORT).show();
+            else {
+                try{
+                    dbrw.execSQL("DELETE FROM myTable WHERE book LIKE '"
+                           +ed_book.getText().toString()+"'");
+                    Toast.makeText(MainActivity.this,"刪除書名"+ed_book.getText().toString(),Toast.LENGTH_SHORT).show();
+                    ed_book.setText("");
+                    ed_price.setText("");
+                }catch (Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this,"新增失敗"+e.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btn_query.setOnClickListener(view -> {
+            Cursor c;
+            if(ed_book.length()<1) c=dbrw.rawQuery("SELECT * FROM myTable",null);
+            else c=dbrw.rawQuery("SELECT * FROM myTable WHERE book LIKE '"+ed_book.getText().toString()+"'",null);
+            c.moveToFirst();
+            items.clear();
+            Toast.makeText(MainActivity.this,"共有"+c.getCount()+"筆",Toast.LENGTH_SHORT).show();
+            for(int i=0;i<c.getCount();i++){
+                items.add("書籍"+c.getString(0)+"\t\t\t\t價格:"+c.getString(1));
+                c.moveToNext();
+            }
+            adapter.notifyDataSetChanged();
+            c.close();
+        });
+
     }
-    
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        dbrw.close();
+    }
 }
--- app/src/main/java/com/example/a112_1_mmslab_java_lab11/MyDBHelper.java
+++ app/src/main/java/com/example/a112_1_mmslab_java_lab11/MyDBHelper.java
@@ -6,18 +6,20 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class MyDBHelper extends SQLiteOpenHelper {
     private static final String name = "mdatabase.db";
-    private static final int version =1;
-    MyDBHelper(Context context){
-        super(context,name,null,version);
+    private static final int version = 1;
+
+    MyDBHelper(Context context) {
+        super(context, name, null, version);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        db.execSQL("CREAT TABLE mytable(book text PRIMARY KEY,price integer NOT NULL)";
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE mytable(book text PRIMARY KEY,price integer NOT NULL)");
     }
 
+
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS myTable");
         onCreate(db);
     }
